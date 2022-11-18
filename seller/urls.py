4fc44c68b84958169334af291b65b50c13b70b96
git-4bckd6 <git-backend46@mathieuholding.com>
2022-11-18 10:55:02 +0500

    feat: create products page

--- seller/urls.py
+++ seller/urls.py
@@ -7,4 +7,9 @@ app_name = 'seller'
 
 urlpatterns = [
     path('seller_dashboard/', views.seller_dashboard, name='dashboard'),
+
+    path('create/logo/', views.create_logo, name='create-logo'),
+    path('create/template/', views.create_template, name='create-template'),
+    path('create/software/', views.create_software, name='create-software'),
+    path('create/api/', views.create_api, name='create-api'),
 ]
--- seller/views.py
+++ seller/views.py
@@ -1,7 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import Http404
 
-# Create your views here.
+
+PRODUCT_TYPES = ["API", 'LOGO', "TEMPLATE", "SOFTWARE"]
 
 def seller_dashboard(request):
     return render(request, 'seller/dashboard.html')
 
+
+def create_logo(request):
+    if request.method == 'GET':
+        return render(
+            request,
+            'seller/create-logo.html'
+            )
+    if request.method == 'POST':
+        print('PRODUCT SUBMITTED')
+        return redirect('seller:dashboard')
+
+
+def create_template(request):
+    if request.method == 'GET':
+        return render(
+            request,
+            'seller/create-template.html'
+            )
+    if request.method == 'POST':
+        print('PRODUCT SUBMITTED')
+        return redirect('seller:dashboard')
+
+
+def create_software(request):
+    if request.method == 'GET':
+        return render(
+            request,
+            'seller/create-software.html'
+            )
+    if request.method == 'POST':
+        print('PRODUCT SUBMITTED')
+        return redirect('seller:dashboard')
+
+
+def create_api(request):
+    if request.method == 'GET':
+        return render(
+            request,
+            'seller/create-api.html'
+            )
+    if request.method == 'POST':
+        print('PRODUCT SUBMITTED')
+        return redirect('seller:dashboard')
