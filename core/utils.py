c6202cffa7d33af40b5288c156ae3afe0350f7db
farzan Ul Haq <farzanulhaq123@gmail.com>
2022-12-17 22:32:11 +0500

    feat: product detail

---
+++ core/utils.py
@@ -0,0 +1,17 @@
+from core.models import Product, ApiService, Logo, HtmlTemplate, DownloadSoftware
+
+
+def get_product_object(product, view):
+    if product.product_type == "A":
+        obj = ApiService.objects.get(product=product)
+        template_name = "core/view-api.html"
+    elif product.product_type == 'L':
+        template_name = "core/view-logo.html"
+        obj = Logo.objects.get(product=product)
+    elif product.product_type == 'H':
+        template_name = "core/view-template.html"
+        obj = HtmlTemplate.objects.get(product=product)
+    elif product.product_type == 'D':
+        template_name = "core/view-software.html"
+        obj = DownloadSoftware.objects.get(product=product)
+    return obj, template_name
--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.conf import settings
+from core.models import Product, ApiService, Logo,  \
+    HtmlTemplate, DownloadSoftware
+from core.utils import get_product_object
 
 def index(request): # landing page
     """Landing Page"""
@@ -17,6 +20,29 @@ def explore(request): # this contains the list of products
     """This contains the list of products"""
     return render(request, 'core/explore.html')
 
+
+def search(request, search):
+    return render(request, 'core/explore.html')
+
+
+def view_product(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    obj, template = get_product_object(product)
+
+    return render(request, template, {
+        'product': product,
+        "obj": obj,
+    })
+
+
+def billing(request): # this contains the list of products
+    """This contains all the transaction history of user"""
+    return render(request, 'core/billing.html')
+
+
+def notifications(request): # this contains the list of products
+    """This contains all the notifications of user"""
+    return render(request, 'core/notifications.html')
 
 
 def project_plan(request): # ONLY FOR DEVs
