d1b22ef5db16bddef782cce3d3dce53816e9b50b
farzan Ul Haq <farzanulhaq123@gmail.com>
2022-12-18 21:34:47 +0500

    refact: password reset frontend

--- main/urls.py
+++ main/urls.py
@@ -16,24 +16,34 @@ urlpatterns = [
 
     path('api/v1/', include('api.urls')),
 
-    path('reset_password/',
-    auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'),
-    name="reset_password"),
-
-    path('reset_password_sent/',
-        auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'),
-        name="password_reset_done"),
-
-    path('reset/<uidb64>/<token>/',
-    auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_form.html'),
-    name="password_reset_confirm"),
-
-
-    path('reset_password_complete/',
-        auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_done.html'),
-        name="password_reset_complete"),
-
-    # Change Password
+    path(
+        'reset_password/',
+        auth_views.PasswordResetView.as_view(
+            template_name='accounts/password_reset.html'
+        ),
+        name="reset_password"
+    ),
+    path(
+        'reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(
+            template_name='accounts/password_reset_sent.html'
+        ),
+        name="password_reset_done"
+    ),
+    path(
+        'reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name='accounts/password_reset_form.html'
+        ),
+        name="password_reset_confirm"
+    ),
+    path(
+        'reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name='accounts/password_reset_done.html'
+        ),
+        name="password_reset_complete"
+    ),
     path(
         'change-password/',
         auth_views.PasswordChangeView.as_view(
