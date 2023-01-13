4b6cd73b0643a87f66e76109aca05a5d36523bbd
farzan Ul Haq <farzanulhaq123@gmail.com>
2023-01-13 15:58:40 +0500

    feat: scraper test

--- market/urls.py
+++ market/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('brochure/', views.brochure_list, name='brochure-list'),
     path('brochure/<int:pk>/', views.brochure_detail, name='brochure-detail'),
     path('product/<int:pk>/market/', views.marketing_platform_list, name='market-list'),
+    path('scraper/', views.scraper, name='scraper'),
     # path('market/platforms/', views.market_platforms, name='market-platforms'),
 ]
--- market/views.py
+++ market/views.py
@@ -1,8 +1,14 @@
+import time
+import chromedriver_autoinstaller
+from selenium import webdriver
+
+from django.http import HttpResponse
 from django.shortcuts import render
-from core.models import Product, MarketingPlatforms
+from django.utils.html import format_html
+from core.models import Product, MarketingPlatforms, DribbleProduct
+
 
 # Create your views here.
-
 def brochure_list(request):
     """Returns Brochure List"""
     return render(request, 'market/brochure-list.html')
@@ -15,8 +21,26 @@ def marketing_platform_list(request, pk):
     """Returns Marketing Platforms"""
     product = Product.objects.get(id=pk)
     print(product.marketed_on.all())
+    if request.method == 'POST':
+        platform = request.POST.get('platform')
+        
     market_platforms = MarketingPlatforms.objects.all()
     return render(request, 'market/marketing-platform-list.html', {
         'product': product,
         "market_platforms": market_platforms
     })
+
+def scraper(request):
+    options = webdriver.ChromeOptions()
+    options.add_argument(' - incognito')
+    options.add_argument('--headless')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--no-sandbox')
+
+    chromedriver_autoinstaller.install()
+    driver = webdriver.Chrome(options=options)
+    driver.get('https://google.com')
+    file = driver.get_screenshot_as_base64()
+    # DribbleProduct.objects.create('')
+    driver.quit()
+    return HttpResponse(format_html(f'<img src="data:;base64,{file}">'))
