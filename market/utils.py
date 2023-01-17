7d93d6b7da359df0a445928946015ab61a431084
farzan Ul Haq <farzanulhaq123@gmail.com>
2023-01-18 00:21:05 +0500

    feat: commit

--- market/utils.py
+++ market/utils.py
@@ -12,7 +12,6 @@ def upload_product_to_dribble(product, title, description, tags, image, platform
     options.add_argument('--headless')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
-    chromedriver_autoinstaller.install()
 
     driver = webdriver.Chrome(executable_path=chromedriver_autoinstaller.install(), options=options)
     print('1. DRIVER OPEN')
