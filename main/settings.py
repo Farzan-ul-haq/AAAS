ec0651f80ded7ec21a3b77941b09399274f655d8
farzan Ul Haq <farzanulhaq123@gmail.com>
2023-04-05 23:07:17 +0500

    refacT; brochure

--- main/settings.py
+++ main/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        "ENGINE": "django.db.backends.postgresql",
-        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
-        "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
-        "USER": os.environ.get("POSTGRES_USER", "postgres"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
-        "PORT": int(os.environ.get("POSTGRES_PORT", "5432")),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # "ENGINE": "django.db.backends.postgresql",
+        # "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        # "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
+        # "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        # "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
+        # "PORT": int(os.environ.get("POSTGRES_PORT", "5432")),
     }
 }
 
