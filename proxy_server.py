3a5211ecc23f790fd673bae5bf31a742464d58ae
Farzan-ul-haq <farzanulhaq123@gmail.com>
2023-04-29 00:37:27 +0500

    feat: proxy_server

--- proxy_server.py
+++ proxy_server.py
@@ -21,18 +21,72 @@ print('DB CONNECTED')
 
 BLOCKED_URL_LISTS = ['favicon.ico',]
 
+def return_request_response(url, request):
+    if request.method == 'GET':
+        request.get(url, )
+
+
 @app.route('/')
 def hello():
     return 'Flask(WEB PROXY) Server is Running....'
 
 
-@app.route('/test/<path:endpoint_path>')
-@limiter.limit("1 per minute")
+@app.route('/test/<path:endpoint_path>', methods = ['POST', 'GET', 'DELETE', "PUT", 'PATCH'])
+@limiter.limit("5 per minute")
 def test_api_requests(endpoint_path):
-    return endpoint_path
+    """
+    THROTTLE LIMIT FOR TEST ENDPOINTS
+    CHECK AAAS-TOKEN EXISTS
+    DB QUERY TO FETCH BASE_URL
+    EXECUTE THE REQUEST TO SELLER SERVER
+    RETURN SELLER SERVER RESPONSE
+    """
+    t = time()
+    product_id = request.headers.get('aaas-token', "").replace('TEST-', "")
+    if product_id:
+        c = conn.cursor()
+        c.execute(f"""
+            SELECT 
+                P.id, APS.base_url 
+            FROM 
+                "Product" P 
+            INNER JOIN 
+                "ApiService" APS ON(APS.id=P.id)
+            WHERE
+                P.id = {product_id} ;
+        """)
+        try:
+            db_data = c.fetchall()[0]
+            print(db_data)
+            base_url = db_data[1]
+        except Exception as e:
+            print(e)
+            return f"INVALID AAAS-TOKEN"
+        print(db_data)
+        print(request.data)
+        if request.method in ['GET']:
+            data = dict(request.values)
+        elif request.method in ['POST', 'PUT', 'PATCH']:
+            data = request.json
+        else:
+            print(request.method)
+            data = {}
+        r = requests.Session().send(
+            requests.session().prepare_request(
+                requests.Request(
+                    request.method, 
+                    base_url+endpoint_path, 
+                    data=data
+                )
+            )
+        )
+        print(time() - t)
+        return (json.loads(r.content), r.status_code)
+    else:
+        return f"ADD AAAS-TOKEN IN HEADERS"
 
 
-@app.route('/<path:endpoint_path>')
+@app.route('/<path:endpoint_path>', methods = ['POST', 'GET', 'DELETE', "PUT", 'PATCH'])
 def handle_api_requests(endpoint_path):
     """
     CHECKED BLOCKED URLS FIRST
@@ -65,19 +119,32 @@ def handle_api_requests(endpoint_path):
             WHERE token = '{token}';
         """)
         try:
-            data = c.fetchall()[0]
-            base_url = data[8]
+            db_data = c.fetchall()[0]
+            base_url = db_data[8]
         except Exception as e:
             print(e)
             return f"INVALID AAAS-TOKEN"
-        print(data)
-        if data[2] > 0:
-            r = requests.get(
-                base_url+endpoint_path,
+        print(db_data)
+        if db_data[2] > 0:
+            if request.method in ['GET']:
+                data = dict(request.values)
+            elif request.method in ['POST', 'PUT', 'PATCH']:
+                data = request.json
+            else:
+                print(request.method)
+                data = {}
+            r = requests.Session().send(
+                requests.session().prepare_request(
+                    requests.Request(
+                        request.method, 
+                        base_url+endpoint_path, 
+                        data=data
+                    )
+                )
             )
             c.execute(f"""
             UPDATE "ClientPackages"
-            SET normal_requests_left = {data[2]-1}
+            SET normal_requests_left = {db_data[2]-1}
             WHERE token = '{token}';
             """)
             conn.commit()
@@ -86,7 +153,7 @@ def handle_api_requests(endpoint_path):
         else:
             return f"No Requests Available. Please Reactivate the package."
     else:
-        return f"NO TOKEN FOUND"
+        return f"ADD AAAS-TOKEN IN HEADERS"
 
 
 if __name__ == '__main__':
