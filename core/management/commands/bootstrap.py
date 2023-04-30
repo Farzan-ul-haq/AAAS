81dea92b1d75bc7067cd29c7bbd2b5f53c1c3261
Farzan-ul-haq <farzanulhaq123@gmail.com>
2023-04-30 22:02:57 +0500

    feat: bootstrap

--- core/management/commands/bootstrap.py
+++ core/management/commands/bootstrap.py
@@ -4,10 +4,12 @@ from datetime import datetime
 
 from django.core.management.base import BaseCommand
 from django.core.mail import send_mail
+from django.core.files import File
 
 from django.conf import settings
 from seller.utils import create_package_obj
-from core.models import Product, User, Logo, MarketingPlatforms
+from core.models import Product, User, Logo, MarketingPlatforms, \
+    BrochureTemplates
 
 REGEX_QUERY = r'"viewsCount":\s*([^"]+)'
 
@@ -19,9 +21,16 @@ class Command(BaseCommand):
             image_data = base64.b64encode(img_file.read())
         return image_data
 
-    def handle(self, *args, **options):
-        Product.objects.all().delete()
 
+    def handle(self, *args, **options):
+        self.create_marketing_platforms()
+        self.create_users()
+        self.create_brochure_templates()
+
+        user = User.objects.first()
+        # logo Product
+
+    def create_marketing_platforms(self):
         MarketingPlatforms.objects.create(
             title="Dribble",
             description="",
@@ -32,73 +41,35 @@ class Command(BaseCommand):
             title="Dribble-PRO",
             description="",
             supported_products="Logo, Web Template, Software",
-            price=0
+            price=1
         )
 
-        user = User.objects.first()
-        # logo Product
-        logo1 = Product.objects.create(
-            owner=user,
-            product_type="L",
-            title="Test Logo 1",
-            description="<p>Test Description for Logo 1</p>",
-            thumbnail_metadata=[
-                {
-                    'data': self.get_image_data('01.png'),
-                    'is_primary': True
-                },
-                {
-                    'data': self.get_image_data('02.png'),
-                    'is_primary': False
-                },
-                {
-                    'data': self.get_image_data('03.png'),
-                    'is_primary': False
-                },
-            ],
-            source_url = "https://google.com",
-            review_count=0,
-            status="A"
+    def create_brochure_templates(self):
+        bt = BrochureTemplates.objects.create(
+            product_type='L',
+            primary_color='blue',
+            secondary_color='black', 
+            image=File(open('static/b1.png', 'rb'))
         )
-        create_package_obj(
-            service=logo1,
-            title='BASIC',
-            price=10,
-        )
-        Logo.objects.create(
-            product=logo1,
-            source_file=self.get_image_data("01.png"),
-            source_file_size="0kb",
-            width=10,
-            height=10,
-            logo_type="SQUARE"
+        bt = BrochureTemplates.objects.create(
+            product_type='L',
+            primary_color='black',
+            secondary_color='black',
+            image=File(open('static/b1-black.png', 'rb'))
         )
 
-        logo2 = Product.objects.create(
-            owner=user,
-            product_type="L",
-            title="Test Logo 2",
-            description="<p>Test Description for Logo 2</p>",
-            thumbnail_metadata=[
-                {
-                    'data': self.get_image_data("01.png"),
-                    'is_primary': True
-                }
-            ],
-            source_url = "https://google.com",
-            review_count=0,
-            status="A"
+    def create_users(self):
+        seller = User.objects.create_superuser(
+            'admin-s',
+            'admin3143',
+            mode='S'
         )
-        create_package_obj(
-            service=logo2,
-            title='BASIC',
-            price=10,
+        seller.save()
+
+        buyer = User.objects.create_superuser(
+            'admin-b',
+            'admin3143',
+            mode='B'
         )
-        Logo.objects.create(
-            product=logo1,
-            source_file=self.get_image_data("01.png"),
-            source_file_size="0kb",
-            width=10,
-            height=10,
-            logo_type="SQUARE"
-        )
+        buyer.mode = 'B'
+        buyer.save()
